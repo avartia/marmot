b2819c266b0a6b18a14b70d25218f5b3f48ce1be
FoolEdward <676748318cyq@gmail.com>
2017-02-28 20:10:43 +0800

    modify

--- src/pages/worldmorph/services/morph.interface.ts
+++ src/pages/worldmorph/services/morph.interface.ts
@@ -4,6 +4,7 @@ import {ColorInterface} from "./color.interface"
 import {MenuMorphInterface} from './menumorph.interface'
 import {PointInterface} from './point.interface'
 import {ShadowMorphInterface} from './shadowmorph.interface'
+import {WorldMorphInterface} from './worldmorph.interface'
 
 //Morph Class
 
@@ -239,48 +240,246 @@ export interface MorphInterface extends NodeInterface{
   //Morph full image(?)
   fullImage():HTMLImageElement;
 
-  //Morph shadow()
+  //Morph shadow(?)
   shadowImage(off:PointInterface, 
               color:ColorInterface):HTMLCanvasElement;
   
-  //Morph shadow()
+  //Morph shadow(produce shadow morph)
   shadowImageBlurred(off:PointInterface, 
                      color:ColorInterface):HTMLCanvasElement;
   
-  //Morph shadow()
+  //Morph shadow(create and set shadow morph)
   shadow(off:PointInterface,
          alpha:number, 
          color:ColorInterface):ShadowMorphInterface;
   
-  //Morph shadow()
+  //Morph shadow(add shadowmorph to a morph)
   addShadow(off:PointInterface,
          alpha:number, 
          color:ColorInterface):ShadowMorphInterface;
 
-  //Morph shadow()
+  //Morph shadow(get shadowmorph which belongs to a morph)
   getShadow():ShadowMorphInterface;
 
-  //Morph shadow()
+  //Morph shadow(remove shadowmorph which belongs to a morph)
   removeShadow():void;
   
+  //Morph pen trails
+  //answer my pen trails canvas. default is to answer my image
+  penTrails():HTMLCanvasElement;
+
+  //Morph updating(record changes for morphs without children)
+  changed():void;
+
+  //Morph updating(record full changes for morphs with children)
+  fullChanged():void
+
+  //Morph updating
+  // react to a change in one of my children,
+  // default is to just pass this message on upwards
+  // override this method for Morphs that need to adjust accordingly
+  childChanged():void;
    
+  //Morph accessing(get worldmorph which contains the morph)
+  world():WorldMorphInterface;
+
+  //Morph accessing(add new morph and remove old parent)
+  add(childMorph):void;
+
+  //Morph accessing(add new morph and remove old parent)
+  //move child to the first member of its children
+  addBack(childMorph):void;
+
+  //Morph accessing(get top layer of morph)  
+  topMorphAt(point:PointInterface):MorphInterface;
+
+  //Morph accessing(get overlapped morph) 
+  overlappedMorphs():MorphInterface[];
+
+  //Morph pixel access(get color of some pixel in the morph)
+  getPixelColor(aPoint:PointInterface):ColorInterface;
+
+  //Morph pixel access
+  //(answer whether some point is transparent in the morph)
+  isTransparentAt(aPoint:PointInterface):boolean;
+
+  //Morph duplicating(shallow copy without children)
+  copy():MorphInterface;
+
+  /*Morph duplicating
+    Produce a copy of me with my entire tree of submorphs. Morphs
+    mentioned more than once are all directed to a single new copy.
+    Other properties are also *shallow* copied, so you must override
+    to deep copy Arrays and (complex) Objects
+  */  
+  fullCopy():MorphInterface;
   
+  /*Morph duplicating
+    Recursively copy this entire composite morph, recording the
+    correspondence between old and new morphs in the given dictionary.
+    This dictionary will be used to update intra-composite references
+    in the copy. See updateReferences().
 
+    Note: This default implementation copies ONLY morphs. If a morph
+    stores morphs in other properties that it wants to copy, then it
+    should override this method to do so. The same goes for morphs that
+    contain other complex data that should be copied when the morph is
+    duplicated.
+  */
+  //copyRecordingReferences(map:Map<MorphInterface, MorphInterface>):
 
+  /*Morph duplicating
+    Update intra-morph references within a composite morph that has
+    been copied. For example, if a button refers to morph X in the
+    orginal composite then the copy of that button in the new composite
+    should refer to the copy of X in new composite, not the original X.
+  */
+  //updateReferences(map:Map<MorphInterface, MorphInterface>);
 
+  //Morph dragging and dropping
+  //get root morph which contains dragging morph
+  rootForGrab():MorphInterface;
 
+  //Morph dragging and dropping
+  //answer whether a morph can accept drop of another morph
+  wantsDropOf(aMorph:MorphInterface):boolean;  
 
+  //Morph dragging and dropping
+  //pick up a morph
+  pickUp(aWorld:WorldMorphInterface):void;   
+  
+  //Morph dragging and dropping
+  //answer whether a morph has been picked up
+  isPickedUp(aWorld:WorldMorphInterface):void;   
 
+  //Morph dragging and dropping
+  //answer a dictionary specifying where I am right now, so
+  //I can slide back to it if I'm dropped somewhere else
+  situation():{origin:MorphInterface, position:PointInterface};   
+
+  //Morph dragging and dropping
+  //slide back to origin position when dropped somewhere else
+  slideBackTo(situation:{origin:MorphInterface, position:PointInterface},
+              steps:number):void;
+
+  //Morph utilities
+  //do nothing
+  nop():void;
+
+  //Morph utilities
+  //resize morph
+  resize():void;  
+
+  //Morph utilities
+  //add handlemorph for moving morph
+  move():void; 
+
+  //Morph utilities
+  //bubble help content
+  hint(msg:string):void;
+
+  //Morph utilities
+  //inform menu
+  inform(msg:string):void;  
+
+  /*Morph utilities
+    inform menu
+    parameters:
+    msg:title
+    callback:function to be executed
+    environment:morph prompt menu belongs to
+    defaultContents
+    width
+    floorNum:minimum number
+    ceilingNum:maximum number
+    isRounded:whether input field value is rounded
+  */
+  prompt(msg:string,
+         callback:Function,
+         environment:MorphInterface,
+         defaultContents:string,
+         width:number,
+         floorNum:number,
+         ceilingNum:number,
+         isRounded:boolean):void;
+
+  /*Morph utilities
+    color menu
+    parameters:
+    msg:title
+    callback:function to be executed
+    environment:morph color menu belongs to
+    defaultContents:default color
+  */
+  pickColor(msg:string,
+            callback:Function,
+            environment:MorphInterface,
+            defaultContents:ColorInterface):void;
+
+  //Morph utilities
+  //inspect menu
+  inspect(anotherObject:MorphInterface):void;
+
+  //Morph menus
+  //answer proper menu
+  contextMenu():MenuMorphInterface;
+
+  //Morph menus
+  //answer hierarchy menu
+  //hierarchyMenu():MenuMorphInterface;
+
+  //Morph menus
+  //answer developers menu
+  //developersMenu():MenuMorphInterface;
+
+  //Morph menus
+  //answer user menu
+  //userMenu():null;
+
+  //Morph menu actions
+  //set transparent degree
+  setAlphaScaled(alpha:number | string):void;
+
+  //Morph menu actions
+  //attach morph to another morph
+  attach():void; 
+
+  //Morph menu actions
+  //toggle draggable property
+  toggleIsDraggable():void;   
+
+  //Morph menu actions
+  //get color setters array
+  colorSetters():string[];
+
+  //Morph menu actions
+  //get number setters array
+  numericalSetters():string[];  
+
+  //Morph entry field tabbing
+  
+  //Morph events
+  //Events may be "bubbled" up a morph's owner chain
+  escalateEvent(functionName:string,
+                arg:any):void
+
+  //Morph eval
+
+  //Morph collision detection
+  //isTouching(otherMorph:MorphInterface):
+  
+  //overlappingImage():
+  
 }
 
 export interface MorphConstructor {
     //constructor of Morph Class
 
-    new (): MorphInterface;
+    new (noDraw:boolean): MorphInterface;
 }
 
 export interface MorphServiceInterface{
 
     //public methods of MorphService
-    create(): MorphInterface;
+    create(noDraw?:boolean): MorphInterface;
 }
