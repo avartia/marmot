32ba6f5253567a1e71d52c7a27fec014675ab6a3
unknown <>
2017-03-04 19:27:04 +0800

    finish penMorph

--- src/pages/canvas/services/penmorph.interface.ts
+++ src/pages/canvas/services/penmorph.interface.ts
@@ -2,23 +2,23 @@
 //PenMorph Class
 
 export interface PenMorphInterface{
-  //public intance member and public intance methods of PenMorph Class
+    //public intance member and public intance methods of PenMorph Class
 
 
-  // PenMorph display:
-  // my orientation can be overridden with the "facing" parameter to
-  // implement Scratch-style rotation styles
-  drawNew(facing:number):void;
+    // PenMorph display:
+    // my orientation can be overridden with the "facing" parameter to
+    // implement Scratch-style rotation styles
+    drawNew(facing:number):void;
 
-  // PenMorph access:
-  setHeading(degrees:number):void;
+    // PenMorph access:
+    setHeading(degrees:number):void;
 
-  turn(degrees:number):void;
+    turn(degrees:number):void;
 
-  //move the turtle forward
-  forward(steps:number):void;
+    //move the turtle forward
+    forward(steps:number):void;
 
-  clear():void;
+    clear():void;
 
 
 
